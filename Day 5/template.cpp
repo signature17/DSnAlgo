b48b16cfdb90054e32abde8e534fe7b9640a0f70
signature17 <paramvir.sin17@gmail.com>
2020-08-21 19:55:06 +0530

    Day5mid

---
+++ Day 5/template.cpp
@@ -0,0 +1,25 @@
+// Do not practice until you get it right, but keep practicing until you can’t get it wrong.
+#include "bits/stdc++.h"
+using namespace std; 
+using ll = long long;
+const ll mod=1e9+7;
+#define take_input freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+#define fast ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+
+ll res;
+
+
+void solve(){
+	GLHF
+}
+ 
+int main(int argc, char const *argv[]){
+	// take_input;
+	fast;
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+    	solve();
+    }
+}
+// GokuOp
